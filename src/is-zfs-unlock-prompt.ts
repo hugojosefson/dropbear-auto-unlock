eb08f488485cd949ffea2d864f3adfa89763b42a
Hugo Josefson <hugo@josefson.org>
2024-11-15 09:47:46 +0100

    chore: a little more lax regex

--- src/is-zfs-unlock-prompt.ts
+++ src/is-zfs-unlock-prompt.ts
@@ -1,5 +1,5 @@
 const ZFS_UNLOCK_PROMPT_REGEXP =
-  /Unlocking encrypted ZFS filesystems\.\.\.\s*Enter the password or press Ctrl-C to exit\.[^A-Za-z0-9]*(Encrypted ZFS password for [^:]+:\s*\(press TAB for no echo\))?\s*$/;
+  /Unlocking encrypted ZFS filesystems\.*\s*Enter the password or press Ctrl-C to exit\.[^A-Za-z0-9]*(Encrypted ZFS password for [^:]+:\s*\(press TAB for no echo\))?\s*$/;
 
 export function isZfsUnlockPrompt(burst: string): boolean {
   return ZFS_UNLOCK_PROMPT_REGEXP.test(burst);
